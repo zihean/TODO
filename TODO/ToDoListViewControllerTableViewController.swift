59d4e52c2162b519d1f7fa0932268fbd6a3302e3
zihean <1104987206@qq.com>
2020-02-13 16:46:33 +0800

    测试表单元显示

--- TODO/ToDoListViewControllerTableViewController.swift
+++ TODO/ToDoListViewControllerTableViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ToDoListViewControllerTableViewController: UITableViewController {
+    
+    let itemArr = ["购买水杯","吃感冒药","修改密码"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,23 +26,24 @@ class ToDoListViewControllerTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return itemArr.count
     }
 
-    /*
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
 
         // Configure the cell...
+        cell.textLabel?.text = itemArr[indexPath.row]
+        cell.backgroundColor = UIColor.systemPink
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
